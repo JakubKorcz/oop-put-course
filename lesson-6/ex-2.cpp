0a4811a83e86e3acb7f434340b98ba8e42d7337a
JakubKorcz <115477480+JakubKorcz@users.noreply.github.com>
2022-11-28 16:37:36 +0100

    Update ex-2.cpp

--- lesson-6/ex-2.cpp
+++ lesson-6/ex-2.cpp
@@ -12,21 +12,43 @@ class Logarithm : public Number {
       try
         {
         return log(this->argument) / log(this->base);
-        if(this->argument!=0)
+        if(this->argument==0)
             {
-                cout<<"Argument is correct";
+                throw argumentequalsnull("The argumnent can't be NULL");
             }
-        else
+        else if(this->base<0)
             {
-                throw 0;
+                throw baseissmallerthannull("The base should be greater than 0");
+            }
+        else if(this->base==1)
+            {
+                throw baseequalsone("The base can't be ONE");
+            }
+        else if(this->argument<0)
+            {
+                throw argumentissmallerthannull("The argument should be greater than 0");
             }
         }
-        catch(int mynum)
+        catch(const argumentequalsnull& e)
         {
             cout<<"The argumnent can't be NULL";
         }
-    }
-}
+        catch(const baseissmallerthannull& e)
+        {
+            cout<<"The base should be greater than 0";
+        }
+        catch(const baseequalsone& e)
+        {
+            cout<<"The base can't be ONE";
+        }
+        catch (const argumentissmallerthannull& e)
+        {
+            cout<<"The argument should be greater than 0";
+        }
+        }
+
+
+
   Logarithm(double inputBase, double inputArgument) {
     this->base = inputBase;
     this->argument = inputArgument;
@@ -38,3 +60,4 @@ int main()
   Logarithm(1,0);
   Logarithm.doubleValue();
 }
+
